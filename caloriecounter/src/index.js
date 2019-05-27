e01c8c5aeeeabe9ac4f086da7d2801be33f876d5
mazdak <nazemi.works@gmail.com>
2019-05-27 05:31:52 -0700

    init config

--- caloriecounter/src/index.js
+++ caloriecounter/src/index.js
@@ -1,1 +1,8 @@
-console.log('It Worked!');
+import initModel from './Model'
+import update from './Update'
+import app from './App'
+import view from './View'
+
+const node = document.querySelector('#app');
+
+app(initModel,update,view,node);
