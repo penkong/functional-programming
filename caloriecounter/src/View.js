8a55ca50862cd06877582f21f57a0fc5bc42c727
mazdak <nazemi.works@gmail.com>
2019-05-27 06:58:15 -0700

    more on form rendering to view on calorie app

--- caloriecounter/src/View.js
+++ caloriecounter/src/View.js
@@ -1,10 +1,37 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom'
 
-const { pre, div, h1, button } = hh(h);
+const { pre, div, h1, button, form, label, input } = hh(h);
+
+//this is for usage in form view func
+function fieldSet(labelText, inputValue) {
+  return div([
+    label({className: 'db mb1'}, labelText),
+    input({
+      className: 'pa2 input-reset ba w-100 mb2',
+      type: 'text',
+      value: inputValue
+    }),
+  ])
+}
+
+function buttonSet(dispatch) {
+  return div([
+    button({className: 'f3 pv2 ph3 bg-blue white bn mr2 dim', type:'submit'}, 'save'),
+    button({className: 'f3 pv2 ph3 bn bg-light-grey dim', type:'button'}, 'cancel'),
+  ])
+}
 
 
 function formView(dispatch, model) {
+  const { description, calories , showForm } = model
+  if(showForm){
+    return form({className: 'w-100 mv-2'},[
+      fieldSet('meal', description),
+      fieldSet('calories', calories || ''),
+      buttonSet(dispatch),
+    ])
+  }
   return button({className: 'f3 pv2 ph3 bg-blue white bn'}, 'add meal')
 }
 
