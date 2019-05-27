8e98eabcfba0de4aeea25f6e1ae7087e10e585ef
mazdak <nazemi.works@gmail.com>
2019-05-27 04:04:34 -0700

    count with hyperscript

---
+++ counter/src/index2.js
@@ -0,0 +1,47 @@
+import hh from 'hyperscript-helpers';
+import { h , diff , patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
+
+
+const { div , button } = hh(h);
+
+const  initModel = 0;
+function view(dispatch, model) {
+  return div([
+    div({className: 'mv2'},`count: ${model}`),
+    button({className: 'pv1 ph2 mr2', onclick: ()=> dispatch(MSG.ADD)}, '+'),
+    button({className: 'pv1 ph2', onclick: ()=> dispatch(MSG.SUBTRACT),'-'})
+  ]);
+}
+
+const MSG = {
+  ADD: 'ADD',
+  SUBTRACT: 'SUBTRACT'
+}
+
+function update(msg,model){
+  switch (msg) {
+    case MSG.ADD:
+      return model + 1;
+    case MSG.SUBTRACT:
+      return model - 1;
+    default:
+      return model;
+  }
+}
+
+function app(initModel,update,view ,node) {
+  let model = initModel;
+  let currentView = view(dispatch,model);
+  let rootNode = createElement(currentView);
+  node.appendChild(rootNode);
+  function dispatch(msg) {
+    model = update(msg,model);
+    const updateView = view(dispatch, model);
+    const patches = diff(currentView, updateView);
+    rootNode = patch(rootNode,patches);
+    currentView = updateView;
+  }
+}
+const rootNode = document.querySelector('#app');
+app(initModel,update,view,rootNode);
